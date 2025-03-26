01b275fc49e27ce6ee0752e312093e67a5deb2d3
Chlebik Krisztofer <susutoo@liba.lol>
2025-03-26 12:20:40 +0100

    na ja

---
+++ Dobozok/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Dobozok {
+    internal class Program {
+        // 5. feladat
+        static List<Doboz> dobozok = new List<Doboz>();
+        
+        static Random random = new Random();
+
+        static void Feladat_3_4(){
+            // 3. feladat
+            Doboz doboz = new Doboz(100, 50, 40);
+
+            // 4. feladat
+            Console.WriteLine("Feladat 4: ");
+            Console.WriteLine("\tTérfogat: " + doboz.Terfogat);
+            Console.WriteLine("\tFelszín: " + doboz.Felszin);
+        }
+
+        static void Feladat_6() {
+            // 6. feladat
+            for (int i = 0; i < 50; i++) {
+                dobozok.Add(
+                    new Doboz(
+                        random.Next(10, 100),
+                        random.Next(10, 100),
+                        random.Next(10, 100)
+                    )
+                );
+            }
+        }
+
+        static void Feladat_7() {
+            // 7. feladat
+            double Atlag = dobozok.Select(x=>x.Terfogat).Sum() / dobozok.Count();
+            Console.WriteLine("Feladat 7:\n\tA dobozok átlagos térfogata: " + Atlag);
+        }
+
+        static void Feladat_8() {
+            // 8. feladat
+            Doboz legnagyobbFelszinu = dobozok.OrderByDescending(x=>x.Felszin).First();
+            Console.WriteLine("Feladat 8:\n\tA legnagyobb felszín értéke: " + legnagyobbFelszinu.Felszin);
+        }
+
+        static void Feladat_9(){
+            // 9. feladat
+            StreamWriter sw = new StreamWriter("dobozok.txt");
+            for (int i = 0; i < dobozok.Count; i++) {
+                sw.WriteLine($"{i+1}. {dobozok[i].Szelesseg},{dobozok[i].Magassag},{dobozok[i].Hosszusag},{dobozok[i].Terfogat},{dobozok[i].Felszin}");
+            }
+            sw.Close();
+        }
+
+        static void Main(string[] args) {
+            Feladat_3_4();
+            Feladat_6();
+            Feladat_7();
+            Feladat_8();
+            Feladat_9();
+        }
+
+        // 1. feladat
+        class Doboz {
+            public double Szelesseg { get; set; }
+            public double Magassag { get; set; }
+            public double Hosszusag { get; set; }
+
+            // 2. feladat
+            public double Terfogat { get; set; }
+            public double Felszin { get; set; }
+
+            public Doboz(double szelesseg, double magassag, double hosszusag) {
+                this.Szelesseg = szelesseg;
+                this.Magassag = magassag;
+                this.Hosszusag = hosszusag;
+
+                // 2. feladat
+                this.Terfogat = szelesseg * magassag * hosszusag;
+                this.Felszin = 2 * (szelesseg * magassag + magassag * hosszusag + hosszusag * szelesseg);
+            }
+        }
+    }
+}
---
+++ alaplapok/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace alaplapok {
+    internal class Program {
+        static List<Alaplap> Alaplapok = new List<Alaplap>();
+
+        static void feladat1()
+        {
+            int alaplapokSzama = Alaplapok.Count;
+            Console.WriteLine($"1. feladat: Összesen {alaplapokSzama} db alaplap van.");
+        }
+
+        static void feladat2()
+        {
+            int szamlalo = 0;
+
+            foreach (Alaplap item in Alaplapok)
+            {
+                if (item.gyarto=="ASUS")
+                {
+                    szamlalo++;
+                }
+            }
+
+            Console.WriteLine($"2. feladat: Összesen {szamlalo} db ASUS-os alaplap van.");
+        }
+
+        static void feladat3()
+        {
+            Console.WriteLine("3. feladat:");
+            foreach (Alaplap item in Alaplapok)
+            {
+                if (item.ar < 100000)
+                {
+                    Console.WriteLine($"   {item.gyarto}   {item.alaplapTipusa}   {item.ar}");
+                }
+            }
+        }
+
+        static void feladat4()
+        {
+            int max = 0;
+            foreach (Alaplap item in Alaplapok)
+            {
+                if (item.ar > max)
+                {
+                    max = item.ar;
+                }
+            }
+            Console.WriteLine($"4. feladat: A legdrágább alaplap ára: {max}");
+        }
+
+        static void feladat5()
+        {
+            Console.WriteLine("5. feladat:");
+            for (int i = Alaplapok.Count - 10; i < Alaplapok.Count; i++) {
+                Console.WriteLine($"  {Alaplapok[i].gyarto}   {Alaplapok[i].alaplapTipusa}   {Alaplapok[i].nepszerusegiMutato}   ");
+            }
+        }
+
+        static void Main(string[] args) {
+            string[] sorok = File.ReadAllLines("alaplapok.txt");
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                string s = sorok[i];
+                
+                Alaplapok.Add(new Alaplap(s));
+            }
+
+            feladat1();
+            feladat2();
+            feladat3();
+            feladat4();
+            feladat5();
+        }
+
+        class Alaplap
+        {
+            // Ország,Gyártó,Alaplap típusa,Ára,Eladott mennyiség,Népszerűségi mutató
+            public string orszag { get; set; }
+            public string gyarto { get; set; }
+            public string alaplapTipusa { get; set; }
+            public int ar { get; set; }
+            public int eladottMennyiseg { get; set; }
+            public double nepszerusegiMutato { get; set; }
+
+            public Alaplap(string orszag, string gyarto, string alaplapTipusa, int ar, int eladottMennyiseg, double nepszerusegiMutato) {
+                this.orszag = orszag;
+                this.gyarto = gyarto;
+                this.alaplapTipusa = alaplapTipusa;
+                this.ar = ar;
+                this.eladottMennyiseg = eladottMennyiseg;
+                this.nepszerusegiMutato = nepszerusegiMutato;
+            }
+
+            public Alaplap(string sor) {
+                string[] x = sor.Split(",");
+                this.orszag = x[0];
+                this.gyarto = x[1];
+                this.alaplapTipusa = x[2];
+                this.ar = int.Parse(x[3]);
+                this.eladottMennyiseg = int.Parse(x[4]);
+                this.nepszerusegiMutato = double.Parse(x[5].Replace(".",","));
+            }
+        }
+    }
+}
+
